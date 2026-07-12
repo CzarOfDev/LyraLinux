64e9e2f644c903d16fddfda5dfde00c6e425c0c0
jetserveDev <mrmopsel@gmail.com>
2026-07-12 23:26:53 +0400

    Branding: Lyra splash, bootloader entries, hostname, lyra user
    - syslinux/efiboot: Arch Linux -> Lyra Linux, removed dead speech/PXE entries
    - syslinux/splash.png: custom Lyra artwork
    - os-release, hostname, hosts
    - live user renamed live -> lyra

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -e -u
 
-# Локаль
+# locale
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 sed -i 's/#\(ru_RU\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-# Live-пользователь
+# Live-user
 groupadd -r autologin
-useradd -m -G wheel,autologin,video,audio,storage -s /bin/bash live
-passwd -d live
+useradd -m -G wheel,autologin,video,audio,storage -s /bin/bash lyra
+passwd -d lyra
 
-# sudo без пароля в live-сессии
+# sudo without password in live-session
 echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/00-lyra-live
 chmod 440 /etc/sudoers.d/00-lyra-live
+
+#Hostname
+
+echo 'lyraiso' > /etc/hostname
