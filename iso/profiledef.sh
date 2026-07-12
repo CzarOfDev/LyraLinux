19ad0a264401f0b86d64ff4d1f61b72e6ef4a810
jetserveDev <mrmopsel@gmail.com>
2026-07-12 17:15:25 +0400

    Branding in profiledef.sh

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_name="lyra"
+iso_label="LYRA_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Lyra Linux <https://github.com/CzarOfDev>"
+iso_application="Lyra Linux Live/Install"
+iso_version="0.1"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
